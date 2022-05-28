735de94a37c7cb770e81fa345c61a16bc9bae3a6
Jae Cho <jjaecho@gmail.com>
2022-05-28 00:35:11 -0400

    Initial Commit

---
+++ P10_CupcakeCorner/P10_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  P10_CupcakeCornerApp.swift
+//  P10_CupcakeCorner
+//
+//  Created by Jae Cho on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct P10_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
