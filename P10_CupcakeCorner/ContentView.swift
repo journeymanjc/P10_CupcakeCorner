283cac6fbc13f43cf4f2e8b7a47ab189df6e8e31
Jae Cho <jjaecho@gmail.com>
2022-05-30 17:05:49 -0400

    Day 50 -1

--- P10_CupcakeCorner/ContentView.swift
+++ P10_CupcakeCorner/ContentView.swift
@@ -11,22 +11,40 @@ import SwiftUI
 
 
 struct ContentView: View {
-	
-	
+	@StateObject var order = Order()
+		
     var body: some View {
-		 AsyncImage(url: URL(string: "https://hws.dev/img/logo.png" )){ phase in
-			 if let image = phase.image {
-				 image
-					 .resizable()
-					 .scaledToFit()
-			 }else if phase.error != nil {
-				 Text("There was an error loading the image")
-			 }else {
-				 ProgressView()
+		 NavigationView{
+			 Form{
+				 Section{
+					 Picker("Select your cake type", selection: $order.type){
+						 ForEach(Order.types.indices){
+							 Text(Order.types[$0])
+						 }
+					 }
+					 Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+				 }
+				 Section{
+					 Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+					 
+					 if order.specialRequestEnabled {
+						 Toggle("Add extra frosting", isOn: $order.extraFrosting)
+						 Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+					 }
+				 }
+				 Section{
+					 NavigationLink{
+						 AddressView(order: order)
+					 } label :{
+						 Text("Detail Details")
+					 }
+				 }
 			 }
+			 .navigationTitle("Cupcake Corner")
 		 }
-		 .frame(width: 200, height: 200)
 	 }
+	
+
 	
 }
 
